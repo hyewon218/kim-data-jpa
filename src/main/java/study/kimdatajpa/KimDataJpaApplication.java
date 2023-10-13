12e0830f1f34111b4ab1307a03cfb1d03ccff519
hyewon218 <won_11111@naver.com>
2023-10-13 21:34:37 +0900

    init commit

---
+++ src/main/java/study/kimdatajpa/KimDataJpaApplication.java
@@ -0,0 +1,13 @@
+package study.kimdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/kimdatajpa/KimDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.kimdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
