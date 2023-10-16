204f3a8827757cac34f97682615bc8574c27dddc
hyewon218 <won_11111@naver.com>
2023-10-16 23:06:27 +0900

    쿼리 메소드 기능 - 메소드 이름으로 쿼리 생성

--- src/main/java/study/kimdatajpa/repository/MemberRepository.java
+++ src/main/java/study/kimdatajpa/repository/MemberRepository.java
@@ -1,7 +1,11 @@
 package study.kimdatajpa.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.kimdatajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    // 스프링 데이터 JPA
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
